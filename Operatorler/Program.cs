15d6aa478adcad1f49aafe5fffde2063b8927789
vtorun <64329150+vtorun@users.noreply.github.com>
2022-02-07 11:03:27 +0300

    Operatorler

---
+++ Operatorler/Program.cs
@@ -0,0 +1,26 @@
+﻿// Atama ve islemli atama
+
+int x = 3;
+int y = 3;
+
+y = y + 2;
+
+Console.WriteLine(y);
+y += 2;
+Console.WriteLine(y);
+y /= 1;
+Console.WriteLine(y);
+x *= 2;
+Console.WriteLine(x);
+
+
+
+bool isSucces = true;
+bool isCompleted = false;
+
+if (isSucces && isCompleted)
+    Console.WriteLine("Perfect");
+if (isSucces || isCompleted)
+    Console.WriteLine("Great");
+if (isSucces && !isCompleted)
+    Console.WriteLine("Fine");
