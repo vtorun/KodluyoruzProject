37ff0e526fe727fb959cddad1edfc13ab86063f1
vtorun <64329150+vtorun@users.noreply.github.com>
2022-02-17 16:18:08 +0300

    Add files via upload
    ArrayList

---
+++ ArrayList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+ArrayList liste = new ArrayList();
+liste.Add("Volkan");
+liste.Add(10);
+liste.Add(true);
+liste.Add('N');
+
+
+Console.WriteLine(liste[0]);
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("**********");
+string[] renkler = { "Yeşil", "Beyaz", "Mavi", "Kırmızı" };
+List<int> sayilar = new List<int>() { 1, 2, 56, 4, 66, 8 };
+liste.AddRange(renkler);
+liste.AddRange(sayilar);
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+//liste.Sort(); //Bütün elemanların aynı değişken türünde olması gerekir.
+//liste.BinarySearch(3); //Sort işleminden sonra kullanılır.
+//liste.Reverse(); // Listeyi ters sıralar
+//liste.Clear(); // Bütün listeyi siler.
