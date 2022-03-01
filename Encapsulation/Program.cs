710888d9d4c7bf967b008528ea423270e549a88e
vtorun <64329150+vtorun@users.noreply.github.com>
2022-03-01 11:28:45 +0300

    Add files via upload
    commit

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,79 @@
+﻿Ogrenci ogrenci = new Ogrenci();
+ogrenci.Isim = "Volkan";
+ogrenci.Soyisim = "Torun";
+ogrenci.OgrenciNo = 123;
+ogrenci.Sinif = 8;
+ogrenci.OgrenciBilgileriniGetir();
+
+ogrenci.SinifAtlat();
+ogrenci.OgrenciBilgileriniGetir();
+
+Ogrenci ogrenci1 = new Ogrenci("Nurcan", "Torun", 123, 1);
+ogrenci1.OgrenciBilgileriniGetir();
+
+ogrenci1.SinifDusur();
+ogrenci1.OgrenciBilgileriniGetir();
+
+class Ogrenci
+{
+    private string isim; //field
+
+    private string soyisim;
+    private int ogrenciNo;
+    private int sinif;
+
+    public string Isim
+    {
+        get { return isim; }
+        set { isim = value; }
+    }
+    public string Soyisim { get => soyisim; set => soyisim = value; }
+    public int OgrenciNo { get => ogrenciNo; set => ogrenciNo = value; }
+    public int Sinif
+    {
+        get => sinif;
+        set
+        {
+            if (value >= 1)
+            {
+                Console.WriteLine("Sınıf en az 1 olabilir.");
+                sinif = 1;
+            }
+            else
+            {
+                sinif = value;
+            }
+        }
+    }
+
+    public Ogrenci(string ısim, string soyisim, int ogrenciNo, int sinif)
+    {
+        Isim = ısim;
+        Soyisim = soyisim;
+        OgrenciNo = ogrenciNo;
+        Sinif = sinif;
+    }
+
+    public Ogrenci()
+    {
+
+    }
+
+    public void OgrenciBilgileriniGetir()
+    {
+        Console.WriteLine("***** Öğrenci Bilgileri *****");
+        Console.WriteLine("Öğrenci Adı : {0}", this.Isim);
+        Console.WriteLine("Öğrenci Soyadı : {0}", this.Soyisim);
+        Console.WriteLine("Öğrenci Numarası : {0}", this.OgrenciNo);
+        Console.WriteLine("Öğrenci Sınıfı : {0}", this.Sinif);
+
+    }
+    public void SinifAtlat()
+    {
+        this.Sinif++;
+    }
+    public void SinifDusur()
+    {
+        this.Sinif--;
+    }
+}
